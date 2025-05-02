dea694910c16f77b63c031ba39b4fe6eb41a8f1d
Dev-Nitish7 <nitishprasad6010@gmail.com>
2025-05-02 14:24:04 +0530

    add rejectUnauthorized to resolve ssl/tcp error on deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -10,6 +10,9 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false, // Accept self-signed certificates (use true with proper certs in production)
+  },
 });
 
 export default pool;
